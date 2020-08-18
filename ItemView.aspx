fd60ecd3258615c1870482b886f2f293683f513a
jiangfan <jiangfan@192.168.1.3>
2020-08-18 09:51:45 +0800

    add itemview to git

---
+++ ItemView.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ItemView.aspx.cs" Inherits="dztzPro.ItemView" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <%= Content %>
+    <input type="hidden" id="jsonContent" value="<%= JsonContent %>" />
+    <input type="hidden" id="ledgerItemId" value="<%= LedgerItemId %>" />
+    <input type="hidden" id="ledgerNodeId" value="<%= LedgerNodeId %>" />
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="HeaderContent" runat="server">
+    <li>
+        <a class="btn btn-success radius mr-20" style="line-height: 1.8em; margin-top: 3px;" href="#" onclick="returntosearch(<%= LedgerNodeId %>);">
+            <i class="icon-ok"></i>返回
+        </a>
+    </li>
+    <li>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+    </li>
+    <li style="color:#040038">
+        <%=Title %>
+    </li>
+    <li>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+    </li>
+
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $("input").attr("disabled", true);
+                $("textarea").attr("disabled", true);
+            });
+    </script>
+</asp:Content>
---
+++ ItemView.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace dztzPro
+{
+    public partial class ItemView : System.Web.UI.Page
+    {
+        public string LedgerItemId { get; set; }
+        public string LedgerNodeId { get; set; }
+        public string Content { get; set; }
+
+        public string JsonContent { get; set; }
+        public string Title { get; set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                JsonContent = "";
+                DztzDataContext dbContext = new DztzDataContext();
+                var id = Convert.ToInt32(Request["ledgerNodeId"]);
+                var itemId = Convert.ToInt32(Request["ledgerNodeItemId"]);
+                var node = dbContext.LedgerNodes.First(ln => ln.Id == id);
+                if (node != null)
+                {
+                    LedgerNodeId = id.ToString();
+                    Content = node.TemplateContent;
+                    Global.CurrentSelectedLedger = id;
+                }
+
+                var nodeItem = dbContext.LedgerNodeItems.FirstOrDefault<LedgerNodeItem>(ln => ln.LedgerNodeId == id && ln.Id == itemId);
+                if (nodeItem != null)
+                {
+                    LedgerItemId = nodeItem.Id.ToString();
+                    JsonContent = HttpUtility.UrlEncode(nodeItem.TemplateValue, Encoding.UTF8);
+                }
+                else
+                {
+                    Response.Redirect("task.aspx?action=1&ledgerNodeId=" + id.ToString());
+                }
+
+                Title = node.LedgerNodeType + "  --  " + node.LedgerNodeName + "  --  查看";
+            }
+            catch(Exception exp)
+            {
+                Console.Out.WriteLine(exp.ToString());
+            }
+        }
+
+        protected void Return_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("search.aspx?action=1&ledgerNodeId=" + LedgerNodeId.ToString());
+        }
+    }
+}
